926cadd7ca2754dc5bf3d7ec4a42b101e19ac5a8
David Siorpaes <david.siorpaes@gmail.com>
2020-01-16 19:08:42 +0100

    Added filename generation based on current date and hour

--- rpidumper/rpidumper.pl
+++ rpidumper/rpidumper.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # Runs tcpdump on a daily basis
 
-#use strict;
+use strict;
 use warnings;
 
 sub midnight_seconds {
@@ -14,17 +14,19 @@ sub midnight_seconds {
 # Evaluate how many seconds we have to run tcpdump
 my $tomidnight = (24 * 3600 - midnight_seconds());
 
-# Prepare file name (TODO)
-my $filename = "temp.pcap";
+# Prepare file name
 # See https://www.tutorialspoint.com/perl/perl_date_time.htm
 my @months = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
-my @days = qw(Sun Mon Tue Wed Thu Fri Sat Sun);
+my @days = qw(Sun Mon Tue Wed Thu Fri Sat Sun); 
+my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime();
+$year += 1900;
+my $time = sprintf ("%02d:%02d:%02d", $hour, $min, $sec);
 
-($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
-print "$mday $months[$mon] $days[$wday]\n";
+my $filename = "$days[$wday]-$mday-$months[$mon]-$year--$time.pcap";
+print "Dumping to " . $filename . "\n";
 
 # Prepare command
-my $command = "timeout $tomidnight tcpdump -i eth0 -r $filename";
+my $command = "timeout $tomidnight tcpdump -n -i eth0 port 9100 -w $filename";
 
 # Ececute it
 print "Executing $command" . "\n";
