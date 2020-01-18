a3ee605810b1226e66ec45249aa64a0956afd490
David Siorpaes <david.siorpaes@gmail.com>
2020-01-18 11:42:24 +0100

    Added dump compression. Minor edits

--- rpidumper/rpidumper.pl
+++ rpidumper/rpidumper.pl
@@ -1,9 +1,10 @@
 #!/usr/bin/perl
-# Runs tcpdump on a daily basis
+# Runs tcpdump on a daily basis and posts the generatad dump file to a server
 
 use strict;
 use warnings;
 
+# Returns elapsed seconds since midnight
 sub midnight_seconds {
    my @time = localtime();
    my $secs = ($time[2] * 3600) + ($time[1] * 60) + $time[0];
@@ -26,18 +27,18 @@ while(1){
    my $filename = "$days[$wday]-$mday-$months[$mon]-$year--$time.pcap";
    print "Dumping to: " . $filename . "\n";
 
-   # Prepare tcpdump command
-   #my $tcpdumpcommand = "timeout $tomidnight tcpdump -n -i eth0 port 9100 -w $filename";
-   my $tcpdumpcommand = "timeout 60 tcpdump -n -i eth0 tcp -w $filename";
-
-   # Ececute it
+   # Prepare and execute tcpdump command
+   my $tcpdumpcommand = "timeout $tomidnight tcpdump -n -i eth0 tcp -w $filename";
    print "Executing: $tcpdumpcommand" . "\n";
    system $tcpdumpcommand;
 
-   # Transfer dump file to server (TODO)
+   # Transfer dump file to server
    my $curlcommand = "curl -F rdata=@" . $filename . " www.nuvolau.com/rsnooper/rsnooper.php";
    print "Executing: $curlcommand" . "\n";
    system $curlcommand;
+
+   # Compress local dump
+   system "gzip $filename";
 
    # Wait before looping back
    print "Waiting...\n";
